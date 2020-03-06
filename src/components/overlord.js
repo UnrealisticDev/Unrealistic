74ba98448267d282d76ee7ff7af5d8c407b99aea
Mustafa <mustafa.moiz125@gmail.com>
2020-03-06 15:08:27 -0800

    update logo and favicon

--- src/components/overlord.js
+++ src/components/overlord.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import {Helmet} from 'react-helmet'
+import React from "react"
+import { Helmet } from "react-helmet"
 
-import Header from './header'
-import Footer from './footer'
+import Header from "./header"
+import Footer from "./footer"
 
-import {Container} from 'react-bootstrap'
+import { Container } from "react-bootstrap"
 
 import "bootstrap/dist/css/bootstrap.css"
-import styles from './overlord.module.css'
+import styles from "./overlord.module.css"
 
-export default ({children}) => (
-    <div className={styles.Body} style={{minHeight: '100vh'}}>
-        <Container fluid className={styles.Content + ' m-0 p-0'}>
-            <Header/>
-            <Container fluid className='mx-0 py-2 px-5'>
-                {children}
-            </Container>
-        </Container>
-        <Footer className={styles.Footer}/>
-    </div>
+import favicon16 from "../images/favicon16.png"
 
+export default ({ children }) => (
+  <div className={styles.Body} style={{ minHeight: "100vh" }}>
+    <Helmet
+      link={[
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: `${favicon16}`,
+        },
+      ]}
+    />
+    <Container fluid className={styles.Content + " m-0 p-0"}>
+      <Header />
+      <Container fluid className="mx-0 py-2 px-5">
+        {children}
+      </Container>
+    </Container>
+    <Footer className={styles.Footer} />
+  </div>
 )
