f1d20930ea30f7f8549cf2720a982d480535f3fc
Mustafa <mustafa.moiz125@gmail.com>
2020-03-09 12:14:02 -0700

    Update post-basic.js

--- src/templates/post-basic.js
+++ src/templates/post-basic.js
@@ -37,7 +37,7 @@ export default ({ data }) => {
                 <div style={{ position: "relative" }}>
                   <Card.Img
                     variant="top"
-    {*/src={image.file.url}*/}
+    {/*src={image.file.url}*/}
                     alt=""
                     fluid
                     style={{
