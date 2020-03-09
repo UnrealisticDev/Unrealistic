eb1d9aab47d5c4b8ec5778993ca15ede7b0b37ba
Mustafa <mustafa.moiz125@gmail.com>
2020-03-09 15:05:50 -0700

    Update articles.js

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -12,9 +12,9 @@ export default ({ data }) => {
       <div class='columns is-multiline is-desktop'>
         {data.allContentfulBlogPost.nodes.map(({ slug, title, image }) => {
           return (
-            <div class='column is-4 is-primary'>
+            <div class='column is-4'>
                 <Link to={slug}>
-                  <div class='card is-primary'>
+                  <div class='card has-background-dark has-text-primary'>
                     <div class="card-image">
                       <figure class="image is-4by3">
                         <img src={image ? image.file.url : "https://versions.bulma.io/0.5.3/images/placeholders/1280x960.png"} alt="Placeholder image" style={{objectFit: 'cover'}}/>
