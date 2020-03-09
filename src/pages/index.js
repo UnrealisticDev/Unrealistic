5e0075de3de0d20a1ae6e54facf6db1fe0dfa0a2
Mustafa <mustafa.moiz125@gmail.com>
2020-03-09 15:53:19 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,26 @@ import React from "react"
 import { graphql } from "gatsby"
 import * as random from "random"
 
+import "../styles/global.scss"
 import "bootstrap/dist/css/bootstrap.css"
-import { Tile, Heading, Card, Container, Content} from "react-bulma-components"
 
 import Overlord from "../components/overlord"
 
+
+
 export default ({ data }) => {
 
-  console.log(data.allPosts);
-  // function getNewestPost() {
-  //   var closestDiff = 99999999
-  //   var closestDiffIdx = -1
-  //   for (var i = 0; i < data.allPosts.edges.length; ++i) {
-  //     var post = data.allPosts.edges[i].node
-  //     if (post.createdAt !== null && Number(post.createdAt) < closestDiff) {
-  //       closestDiff = post.createdAt
-  //       closestDiffIdx = i
-  //     }
-  //   }
+ // console.log(data.allPosts);
+ // function getNewestPost() {
+ //      var closestDiff = 99999999
+ //      var closestDiffIdx = -1
+ //      for (var i = 0; i < data.allPosts.edges.length; ++i) {
+ //      var post = data.allPosts.edges[i].node
+ //      if (post.createdAt !== null && Number(post.createdAt) < closestDiff) {
+ //        closestDiff = post.createdAt
+ //        closestDiffIdx = i
+ //      }
+ //    }
 
   //   return data.allPosts.edges[closestDiffIdx].node
   // }
@@ -43,71 +45,36 @@ export default ({ data }) => {
   //   return data.allPosts.edges[idx].node
   // }
 
-  // var freshPost = getNewestPost()
+  //  var freshPost = getNewestPost()
   // var insiderPost = getNewestInsiderPost()
 
   return (
     <Overlord>
-      <Tile kind="ancestor">
-        <Tile>
-          <Tile vertical>
-            <Tile>
-              <Tile size={9}>
-                <Tile kind="parent">
-                  <Tile renderAs="article" kind="child">
-                    <Content>
-                      {/* <img src={freshPost.image.file.url} style={{margin: 0}}/> */}
-                    </Content>
-                  </Tile>
-                </Tile>
-              </Tile>
-              <Tile vertical kind="parent">
-                <Tile renderAs="article" kind="child">
-                  <img src="https://images.wallpaperscraft.com/image/texture_relief_3d_156496_1920x1080.jpg" />
-                  <Heading>Titles</Heading>
-                  <Heading subtitle>Lols</Heading>
-                </Tile>
-                <Tile renderAs="article" kind="child">
-                  <img src="https://images.wallpaperscraft.com/image/texture_relief_3d_156496_1920x1080.jpg" />
-                </Tile>
-              </Tile>
-            </Tile>
-            <Tile kind="parent">
-              <Tile renderAs="article" kind="child" size={4}>
-                <img src="https://i.ytimg.com/vi/Yf9tQKheHbk/maxresdefault.jpg" />
-              </Tile>
-              <Tile renderAs="article" kind="child">
-                <img
-                  src="https://images.wallpaperscraft.com/image/texture_relief_3d_156496_1920x1080.jpg"
-                  style={{ objectFit: "cover" }}
-                />
-              </Tile>
-            </Tile>
-          </Tile>
-          <Tile kind="parent" size={3}>
-            <Tile kind="child">
-              <Container // tip of the week
-                className="align-self-center px-lg-3 px-xs-0"
-                style={{ color: "#404040" }}
-              >
-                <h2
-                  style={{
-                    textAlign: "center",
-                    width: "100%",
-                    borderBottom: "2px solid #EAAA03",
-                    color: "#FF9900",
-                  }}
-                >
-                  Tip of the Week
-                </h2>
-                <p style={{ textAlign: "justify" }} className="h-100">
-                  {data.site.siteMetadata.tipOfTheWeek}
-                </p>
-              </Container>
-            </Tile>
-          </Tile>
-        </Tile>
-      </Tile>
+      <div class='tile is-ancestor'>
+        <div class='tile'>
+          <div class='tile is-parent is-9'>
+            <div class='tile is-child box'>
+              1
+            </div>
+          </div>
+          <div class='tile is-parent is-vertical'>
+            <div class='tile is-child box'>
+              2
+            </div>
+            <div class='tile is-child box'>
+              3
+            </div>
+          </div>
+        </div>
+        <div class='tile is-parent'>
+          <div class='tile is-child box is-4'>
+            4
+          </div>
+          <div class='tile is-child box is-8'>
+            5
+          </div>
+        </div>
+      </div>
     </Overlord>
   )
 }
