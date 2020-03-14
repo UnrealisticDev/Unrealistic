bf7162f36970bdd72c0536c34415e053f4c14d99
Mustafa <mustafa.moiz125@gmail.com>
2020-03-14 03:22:04 -0700

    fixup logo and favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,17 +5,25 @@ import Header from "./header"
 import Footer from "./footer"
 
 import favicon16 from "../images/favicon16.png"
+import favicon32 from "../images/favicon32.png"
+import favicon64 from "../images/favicon64.png"
+
+function getFavicon(size, icon) {
+  return {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: size.toString() + 'x' + size.toString(),
+    href: `${icon}`
+  }
+}
 
 export default ({ children, title }) => (
   <>
     <Helmet
       link={[
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "16x16",
-          href: `${favicon16}`,
-        },
+        getFavicon(16, favicon16),
+        getFavicon(32, favicon32),
+        getFavicon(64, favicon64),
       ]}
     >
       <title>{title}</title>
