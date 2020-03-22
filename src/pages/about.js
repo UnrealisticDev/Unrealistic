e39071f2e611ebf9a98cfe7df58a80de90c75c86
Mustafa <mustafa.moiz125@gmail.com>
2020-03-21 17:10:09 -0700

    cleanup hireme form

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,10 +8,34 @@ export default () => (
   <Layout>
     <SEO title="About" />
     <div class="container">
-      <div class="columns">
-        <div class="column is-8"></div>
-        <div class="column is-4">
+      <div class="columns is-centered">
+        <div class="column is-3">
           <Hireme />
+        </div>
+        <div class='column is-1'/>
+        <div class="column is-4">
+          <div class="container">
+            <div class="content">
+              <div className="title">About Unrealistic</div>
+              <p>
+                Unrealistic's mission is to inform, empower, and inspire our
+                game developer readership. We do this through journalism, as
+                well as criticism, and also by providing game developers a
+                prominent platform to explore all matters related to the art and
+                business of making video games via our comment system.
+              </p>
+            </div>
+            <div class="content">
+              <div className="title">About Nokternel Games</div>
+              <p>
+                Everything we do at Nokternel Games is based on the
+                success of the gaming experiences we provide our players. The
+                goal of each discipline within the company -- be it art,
+                programming or customer support -- is to make our games as fun
+                as possible for as many people as we can reach.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
