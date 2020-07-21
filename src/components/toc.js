61f8df05e3d121eaf8dcf514568d52480db2ef7a
Mustafa <mustafa.moiz125@gmail.com>
2020-07-21 13:09:49 -0700

    add expansion toggle to toc

--- src/components/toc.js
+++ src/components/toc.js
@@ -1,15 +1,40 @@
 import React from 'react';
 
 import styles from './toc.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 class TOC extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			expanded: true,
+		};
+		this.toggleExpansion = this.toggleExpansion.bind(this);
+	}
+
+	toggleExpansion(event) {
+		this.setState({ expanded: !this.state.expanded });
+	}
+
 	render() {
 		return this.props.src ? (
 			<>
-				<p className='menu-label'>Contents</p>
-				<ul className={'menu-list ' + styles.List}>
-					<div dangerouslySetInnerHTML={{__html: this.props.src}}/>
-				</ul>
+				<div class='level' onClick={this.toggleExpansion}>
+					<div class='level-left'>
+						<p className='menu-label'>Contents</p>
+					</div>
+					<div className='level-right'>
+						<FontAwesomeIcon
+							icon={this.state.expanded ? faChevronUp : faChevronDown}
+						/>
+					</div>
+				</div>
+				{this.state.expanded && (
+					<ul className={'menu-list ' + styles.List}>
+						<div dangerouslySetInnerHTML={{ __html: this.props.src }} />
+					</ul>
+				)}
 			</>
 		) : null;
 	}
