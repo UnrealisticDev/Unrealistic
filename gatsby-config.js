db9de500b5e3e509cb13e30abd345332579bef60
Mustafa <mustafa.moiz125@gmail.com>
2020-08-06 10:41:34 -0700

    add basic redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,8 @@ module.exports = {
         ]
       }
     },
+    /* Hosting */
+    `gatsby-plugin-netlify`,
     /* Progressive Web App (PWA) */
     {
       resolve: `gatsby-plugin-manifest`,
