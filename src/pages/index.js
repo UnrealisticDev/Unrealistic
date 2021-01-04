474103ac9047a19934dc51f8bb997e2b1679cdf7
Mustafa <mustafa.moiz125@gmail.com>
2021-01-04 18:58:30 +0500

    Overhauled layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Helmet from "react-helmet";
 import styled from "styled-components";
 
 import Layout from "../components/layout";
@@ -19,8 +20,14 @@ const PageTitle = styled.h1`
     font-weight: 900;
     font-display: auto;
   }
-  font-family: "basic-sans", sans-serif;
-  color: #eaaa03 !important;
+  ${"" /* font-family: "basic-sans", sans-serif; */}
+  font-family: 'Bungee', cursive;
+
+  & #title-remainder {
+    color: hsl(204, 86%, 53%);
+    font-family: "Bungee", cursive !important;
+    font-size: 2rem;
+  }
 `;
 
 const PageSubtitle = styled.h2`
@@ -40,142 +47,57 @@ const PageSubtitle = styled.h2`
 `;
 
 export default ({ data }) => {
-
   return (
-    <Layout>
-      <SEO
-        title="Unrealistic: Game Development Tutorials, Insider Insights, and Industry Analysis"
-        titleOverride
-        description="Unrealistic is the ultimate resource for cracking the gaming industry. It has tutorials on Unreal Engine 4 and Unity, insider insights, and coverage of industry news."
-      />
-      {/* Intro */}
-      <section className="hero is-fullheight-with-navbar is-light">
-        <div className="hero-body">
-          <div className="container">
-            <div className="columns is-vcentered is-variable is-8">
-              <div className="column is-4">
-                <PageTitle className="title is-size-1">Unrealistic</PageTitle>
-                <PageSubtitle className="subtitle">
-                  Game Development Tutorials, Insider Insights, and Industry
-                  Analysis
-                </PageSubtitle>
-                <p>
-                  Unrealistic is the ultimate resource for cracking the gaming
-                  industry. It has tutorials on Unreal Engine 4 and Unity,
-                  insider insights, and coverage of industry news.
-                </p>
-              </div>
-              <div className="column is-8">
-                <Carousel items={data.allPosts} />
-              </div>
-            </div>
+    <>
+      <Helmet>
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Bungee&family=Roboto&display=swap');
+        </style>
+      </Helmet>
+      <Layout>
+        <SEO
+          title="Unrealistic: Game Development Tutorials, Insider Insights, and Industry Analysis"
+          titleOverride
+          description="Unrealistic is the ultimate resource for cracking the gaming industry. It has tutorials on Unreal Engine 4 and Unity, insider insights, and coverage of industry news."
+        />
+        <section
+          className="hero is-fullheight-with-navbar is-light"
+          style={{ maxHeight: "calc(100vh - 3.25rem)", overflow: "hidden" }}
+        >
+          <div
+            className="hero-body is-marginless is-paddingless"
+            style={{ width: "100%", height: '100%', overflow: "hidden" }}
+          >
+            <Carousel items={data.allPosts} />
           </div>
-        </div>
-      </section>
-      {/* Industry */}
-      {/* Insider */}
-      {/* <section className="hero is-dark is-fullheight-with-navbar">
-        <div className="hero-body">
-          <div className="container">Insider</div>
-        </div>
-      </section>
-      <section className="hero is-light is-fullheight-with-navbar">
-        <div className="hero-body">
-          <div className="container">Newsletter</div>
-        </div>
-      </section> */}
-      {/* <section className="hero is-fullheight-with-navbar">
-        <div className="container">
-          <section className="section">
-            <div className="level">
-              <div className="container">
-                <div className="content has-text-centered"></div>
-              </div>
-            </div>
-          </section>
-          <div className="tile is-ancestor">
-            <div className="tile is-vertical">
-              <div className="tile">
-                <div className="tile is-parent is-9">
-                  <div className="tile is-child">
-                    <Widget
-                      title={freshPost.title}
-                      flair="Fresh Off the Press"
-                      subtitle={freshPost.body.childMarkdownRemark.excerpt}
-                      image={getImageFromPost(freshPost)}
-                      to={router.getPostSlug(freshPost.slug)}
-                      fullheight
-                    />
-                  </div>
-                </div>
-                <div className="tile is-parent is-vertical">
-                  <div className="tile is-child">
-                    <Widget
-                      title={stylePost.title}
-                      subtitle="A comprehensive set of style conventions for UE4 projects."
-                      flair="Style"
-                      to={router.getPostSlug(stylePost.slug)}
-                      image={getImageFromPost(stylePost)}
-                      fullheight
-                    />
-                  </div>
-                  <div className="tile is-child">
-                    <TipOfTheWeek className={"content has-text-centered"}>
-                      <hr />
-                      <div className="title" id="totw-title">
-                        Tip of the Week
-                      </div>
-                      <p>{totw.text.text}</p>
-                      <p id="totw-author">{totw.author}</p>
-                      {totw.source && (
-                        <a href={totw.source} style={{ margin: "15px" }}>
-                          <FontAwesomeIcon
-                            icon={faExternalLinkAlt}
-                            id="totw-source"
-                          />
-                        </a>
-                      )}
-                    </TipOfTheWeek>
-                  </div>
-                </div>
-              </div>
-              <div className="tile">
-                <div className="tile is-parent is-vertical is-4">
-                  <div className="tile is-child">
-                    <Widget
-                      to={router.getPostSlug(devlogPost.slug)}
-                      title="Project Ascendant"
-                      subtitle="An upcoming stealth crawler."
-                      flair="Devlog"
-                      image={getImageFromPost(devlogPost)}
-                    />
-                  </div>
-                  <div className="tile is-child">
-                    <Widget
-                      to={router.getPostSlug(getRandomPost().slug)}
-                      title="Random"
-                      subtitle="Learn something new."
-                      image="https://cdn.vox-cdn.com/thumbor/2PaCKdhf1dUhQkcGE9P-pMwKcJQ=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/8587203/overwatch_loot_box.jpg"
-                    />
-                  </div>
-                </div>
-                <div className="tile is-parent">
-                  <div className="tile is-child">
-                    <Widget
-                      to={router.getPostSlug(beginnerPost.slug)}
-                      subtitle="Build a tower defense game from top to bottom in just 21 days."
-                      title="Beginner's Guide"
-                      flair="Tutorials"
-                      image={getImageFromPost(beginnerPost)}
-                    />
-                  </div>
+        </section>
+        {/* Intro */}
+        {/* <section className="hero is-fullheight-with-navbar is-light">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns is-vcentered is-variable is-8">
+                <div className="column is-4">
+                  <PageTitle className="title is-size-1">
+                    <span id="title-lead">Un</span>
+                    <span id="title-remainder">realistic</span>
+                  </PageTitle>
+                  <PageSubtitle className="subtitle">
+                    Game Development Tutorials, Insider Insights, and Industry
+                    Analysis
+                  </PageSubtitle>
+                  <p>
+                    Unrealistic is the ultimate resource for cracking the gaming
+                    industry. It has tutorials on Unreal Engine 4 and Unity,
+                    insider insights, and coverage of industry news.
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section> */}
-    </Layout>
+        </section> */}
+      </Layout>
+    </>
   );
 };
 
@@ -193,6 +115,9 @@ export const query = graphql`
           image {
             fluid(maxWidth: 900) {
               ...GatsbyContentfulFluid
+            }
+            file {
+              url
             }
           }
           excerpt
