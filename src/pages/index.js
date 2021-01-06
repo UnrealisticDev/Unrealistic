a37a64627483d765b6ab30b00a919f1e66f17a51
Mustafa <mustafa.moiz125@gmail.com>
2021-01-06 14:53:32 +0500

    Restyled home page for mobile-friendliness

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
+import Img from "gatsby-image";
 import Helmet from "react-helmet";
 import styled from "styled-components";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Carousel from "../components/carousel";
+import { getPostSlug } from "../scripts/router";
 
 const PageTitle = styled.h1`
   @font-face {
@@ -46,6 +48,39 @@ const PageSubtitle = styled.h2`
   font-family: "basic-sans", sans-serif;
 `;
 
+const MobilePostCard = ({ post }) => {
+  const Wrapper = styled.div`
+    margin-bottom: 2rem;
+  `;
+
+  const StyledLink = styled(Link)``;
+
+  const StyledImg = styled(Img)`
+    border-radius: 5px;
+  `;
+
+  const Title = styled.h2`
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem !important;
+    font-family: "basic-sans", san-serif;
+  `;
+
+  const Excerpt = styled.p`
+    font-family: 'Open Sans', sans-serif;
+  `
+
+  const { slug, title, image, excerpt, body } = post;
+  return (
+    <Wrapper>
+      <StyledLink to={getPostSlug(slug)}>
+        <StyledImg fluid={image ? image.fluid : null} />
+      </StyledLink>
+      <Title className="title is-size-5">{title}</Title>
+      <Excerpt>{excerpt || body.childMarkdownRemark.excerpt}</Excerpt>
+    </Wrapper>
+  );
+};
+
 export default ({ data }) => {
   return (
     <>
@@ -53,6 +88,10 @@ export default ({ data }) => {
         <style>
           @import
           url('https://fonts.googleapis.com/css2?family=Bungee&family=Roboto&display=swap');
+        </style>
+        <style>
+          @import
+          url("https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Open+Sans&display=swap");
         </style>
       </Helmet>
       <Layout>
@@ -62,17 +101,27 @@ export default ({ data }) => {
           description="Unrealistic is the ultimate resource for cracking the gaming industry. It has tutorials on Unreal Engine 4 and Unity, insider insights, and coverage of industry news."
         />
         <section
-          className="hero is-fullheight-with-navbar is-light"
+          className="hero is-fullheight-with-navbar is-light is-hidden-mobile"
           style={{ maxHeight: "calc(100vh - 3.25rem)", overflow: "hidden" }}
         >
           <div
             className="hero-body is-marginless is-paddingless"
-            style={{ width: "100%", height: '100%', overflow: "hidden" }}
+            style={{ width: "100%", height: "100%", overflow: "hidden" }}
           >
             <Carousel items={data.allPosts} />
           </div>
         </section>
         {/* Intro */}
+        <section className="section">
+          <div className="container">
+            {data.allPosts.edges.map(({ node }, i) => (
+              <>
+              <MobilePostCard post={node}/>
+              {i < data.allPosts.edges.length - 1 && <hr style={{background: 'rgb(.1, .1, .1, .1)'}}/>}
+              </>
+            ))}
+          </div>
+        </section>
         {/* <section className="hero is-fullheight-with-navbar is-light">
           <div className="hero-body">
             <div className="container">
